dd30117f049640232649d96ce10508c3fb1f6118
MakicLin <makiclin@gmail.com>
2018-10-05 00:20:09 -0700

    Create 100dots.js

---
+++ 100dots.js
@@ -0,0 +1,21 @@
+var x;
+ var y;
+
+function setup() {
+ createCanvas(400, 400);
+ colorMode(HSB, width, height, 100);
+
+}
+
+
+function draw() {
+
+ for (var i = 0; i <= 100; i = i + 1) {
+  
+ var x = random(width);
+ var y = random(height);
+  stroke(x, y, 100);
+  point(x, y);
+
+ }
+}
