e2ebff4bad43f3082295eb021024b6a994d361cd
MakicLin <makiclin@gmail.com>
2018-10-04 19:04:29 -0700

    Update angle-lines.js

--- angle-lines.js
+++ angle-lines.js
@@ -1,11 +1,12 @@
 function setup() {
   createCanvas(400, 400);
+  colorMode(HSB);
 }
 
 function draw() {
   background(255);
 
-  for (var x = 10; x < width-10; x = x + 40) {
-    line(x, height/2, mouseX, mouseY);
+  for (var x = 10; x < 400; x = x + 10) {
+    line(x, height/2, x+100, height/2-75);
   }
 }
